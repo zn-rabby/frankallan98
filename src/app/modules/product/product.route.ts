b9d66efb48ef802f7c5fa1ef682049ff2bf2664e
Zulkar Naeem Rabby <rabby.webdeveloper@gmail.com>
2025-07-15 18:10:19 +0600

    feat(product): implement product creation with image upload using multipart/form-data

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import { USER_ROLES } from '../../../enums/user';
 import auth from '../../middleware/auth';
 import { ProductController } from './product.controller';
+import fileUploadHandler from '../../middleware/fileUploadHandler';
+import parseFileData from '../../middleware/parseFileData';
 const router = express.Router();
 
-router.post('/', auth(USER_ROLES.USER, USER_ROLES.ADMIN, USER_ROLES.SUPER_ADMIN, USER_ROLES.VENDOR), ProductController.createProduct);
+router.post('/', fileUploadHandler(), parseFileData('image'), auth(USER_ROLES.USER, USER_ROLES.ADMIN, USER_ROLES.SUPER_ADMIN, USER_ROLES.VENDOR), ProductController.createProduct);
 
 export const ProductRoutes = router;
--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -2,11 +2,20 @@ import { StatusCodes } from 'http-status-codes';
 import AppError from '../../../errors/AppError';
 import { IProduct } from './product.interface';
 import { Product } from './product.model';
+import { MongoAPIError } from 'mongodb';
+import unlinkFile from '../../../shared/unlinkFile';
 
 const createProductToDB = async (payload: IProduct): Promise<IProduct> => {
-     const report = await Product.create(payload);
-     if (!report) throw new AppError(StatusCodes.BAD_REQUEST, 'Failed to created Product ');
-     return report;
+     const data = await Product.create(payload);
+     const { image } = payload;
+
+     if (!data) {
+          unlinkFile(image);
+          throw new AppError(StatusCodes.BAD_REQUEST, 'Failed to create Service');
+     }
+
+     if (!data) throw new AppError(StatusCodes.BAD_REQUEST, 'Failed to created Product ');
+     return data;
 };
 
 export const ProductService = {
