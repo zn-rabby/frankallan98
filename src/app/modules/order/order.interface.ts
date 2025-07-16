5e14ffe8994470508886b853a252e12c82cac921
Zulkar Naeem Rabby <rabby.webdeveloper@gmail.com>
2025-07-16 17:19:16 +0600

    feat(order): define order and delivery address types with detailed location fields

---
+++ src/app/modules/order/order.interface.ts
@@ -0,0 +1,33 @@
+import { Model, Types } from 'mongoose';
+export type IDeliveryAddress = {
+     name: string;
+     phone: number;
+     districts: string;
+     city: string;
+     ward: string;
+     subWards: string;
+     plotApartmentRoad: string;
+     deliveryTime: Date;
+     deliveryDate: Date;
+};
+
+export type IOrder = {
+     product: Types.ObjectId;
+     name: string;
+     phone: number;
+     price: number;
+     districts: string;
+     city: string;
+     ward: string;
+     subWards: string;
+     plotApartmentRoad: string;
+     itemDetails: string;
+     pickupTime: Date;
+     serviceType: Types.ObjectId;
+     images: Types.ObjectId;
+     productWeight: number;
+     quantity: number;
+     deliveryAddress: string;
+};
+
+export type ProductModel = Model<IOrder, Record<string, unknown>>;
